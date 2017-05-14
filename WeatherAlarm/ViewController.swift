ad299e076a8001dca6fedad6db0ec829785963d4
TaigaMikami <mikamimikami60@gmail.com>
2017-05-14 13:26:57 +0900

    Initial Commit

---
+++ WeatherAlarm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherAlarm
+//
+//  Created by 三上大河 on 2017/05/14.
+//  Copyright © 2017年 Gutty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
